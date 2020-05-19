85c2a8e8c02071ee2f1ab2525cbd8b2c0502b853
rubycat2002 <65579283+rubycat2002@users.noreply.github.com>
2020-05-19 11:32:35 +0900

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="home">
+    <img src="../assets/logo.png" />
+    <h1>{{ msg }}</h1>
+    <button @click="googleLogin">google login</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      msg: "Welcome to remo"
+    };
+  },
+  methods: {
+    googleLogin: function() {
+      firebase
+        .auth()
+        .signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
